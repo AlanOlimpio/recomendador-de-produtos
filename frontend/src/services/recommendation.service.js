eb427ae914d27536daa23a2fb7864cdbc8d264ba
Alan Olimpio <alancleyton.olimpio@gmail.com>
2025-09-08 18:43:56 -0300

    feat: cria logica para retornar produtos recomendados

--- frontend/src/services/recommendation.service.js
+++ frontend/src/services/recommendation.service.js
@@ -7,6 +7,38 @@ const getRecommendations = (
   /**
    * Crie aqui a lógica para retornar os produtos recomendados.
    */
+
+  if (!products) {
+    return [];
+  }
+
+  const {
+    selectedPreferences = [],
+    selectedFeatures = [],
+    selectedRecommendationType,
+  } = formData;
+  const productsFiltered = products.filter((product) => {
+    const preferences = product.preferences ?? [];
+    const features = product.features ?? [];
+
+    const matchesPreferences =
+      selectedPreferences.length === 0 ||
+      selectedPreferences.some((pref) => preferences.includes(pref));
+
+    const matchesFeatures =
+      selectedFeatures.length === 0 ||
+      selectedFeatures.some((feat) => features.includes(feat));
+
+    return matchesPreferences && matchesFeatures;
+  });
+
+  if (
+    selectedRecommendationType === 'SingleProduct' &&
+    productsFiltered.length > 0
+  ) {
+    return [productsFiltered[productsFiltered.length - 1]];
+  }
+  return productsFiltered;
 };
 
 export default { getRecommendations };
